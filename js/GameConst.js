4f8d9a5e33f86156cf1c10b92704116c9fd97728
Masashi HIJIKATA <masashi.hijikata@gmail.com>
2025-11-12 03:02:28 +0900

    v0.7g - 2025.11.12

--- js/GameConst.js
+++ js/GameConst.js
@@ -1,7 +1,7 @@
 // GameConst.js
 
 export const GLOBALS = {
-    VERSION : "0.7f - 2025.11.12",
+    VERSION : "0.7g - 2025.11.12",
     STAGE_MAX : 8,
     INIT_LIVES : 3,
     EXTEND_FIRST : 50000,
